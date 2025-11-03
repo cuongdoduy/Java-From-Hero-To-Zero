493a831622266557ca0ab24a6029b800ca3c70bf
Gabriel Do <gabriel.do@siliconstack.com.au>
2025-11-03 21:32:15 +0700

    first commit

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        Integer ss = 10;
+       printHelloWorld();
+    }
+}
